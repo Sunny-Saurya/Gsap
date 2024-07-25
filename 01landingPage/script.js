ce4213f1765d0e4f65b40b2147463bd82d16b0dc
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-07-25 16:20:20 +0530

    landing page

---
+++ 01landingPage/script.js
@@ -0,0 +1,24 @@
+gsap.from("#img1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:60,
+})
+gsap.from("#img2",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    x:60,
+})
+gsap.from("#img3",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:-60,
+})
+gsap.from("#main #head",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    
+})
